b844a703e7e531ec67bada16f51e3523d32cf5af
Aarush Saboo <160993404+aarushsaboo@users.noreply.github.com>
2025-04-02 17:12:26 +0530

    fixed navbar

--- app.py
+++ app.py
@@ -94,6 +94,18 @@ def download_image(url):
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/search', methods=['POST'])
 def search():
     query = request.form.get('query')
